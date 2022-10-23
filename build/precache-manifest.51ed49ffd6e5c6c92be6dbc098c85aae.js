26d803413909146f40cf241a9e4299e4ae5b336d
Utkarsh Pathrabe <uapathrabe@gmail.com>
2022-10-23 11:02:10 +0530

    Create precache-manifest.51ed49ffd6e5c6c92be6dbc098c85aae.js

---
+++ build/precache-manifest.51ed49ffd6e5c6c92be6dbc098c85aae.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac173462c5ea25013861b4eec7adc34e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d82a39d35aa9186e33d7",
+    "url": "/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "a3666ab6f1d565fe9ea6",
+    "url": "/static/js/2.1586a46b.chunk.js"
+  },
+  {
+    "revision": "936711c3f4be0c9c88207c43cda44117",
+    "url": "/static/js/2.1586a46b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d82a39d35aa9186e33d7",
+    "url": "/static/js/main.a8ba5cec.chunk.js"
+  },
+  {
+    "revision": "e9eb119b9a822683e34f",
+    "url": "/static/js/runtime-main.b950e041.js"
+  },
+  {
+    "revision": "f78b305a8d59149b2e0855a96022dce7",
+    "url": "/static/media/Quizzer_Logo_01.f78b305a.png"
+  },
+  {
+    "revision": "8b5b29d9ac94e344512ec6e0ae89b347",
+    "url": "/static/media/Quizzer_Logo_02.8b5b29d9.png"
+  }
+]);
